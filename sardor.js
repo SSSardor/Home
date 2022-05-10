75b057c8223c7381e61080e546ea8f26968b2c73
SSSardor <sardorsamijonov1226@gmail.com>
2022-05-10 23:35:03 +0500

    Home CSS

---
+++ sardor.js
@@ -0,0 +1,27 @@
+var name = 'sardor';
+var surname = 'samijonov';
+surname = 'Samijonov';
+console.log(name);
+console.log(surname);
+var max = Number.MAX_SAFE_INTEGER;
+console.log(max);
+var num = 111;
+//----------------2 chi dars js-------------------
+console.log(parseInt(num, 2));
+var num1 = 11.88;
+var num2 = 12.55;
+var num3 = -121;
+console.log(parseInt(num));
+console.log(parseFloat(num1));
+console.log(Math.pow(2, 2)); // daraja
+console.log(Math.round(num2)); // yaxlitlash
+console.log(Math.ceil(num1)); // uzidan katta butun songa utkazadi 3.1 ni ceil qise   4 boladi
+console.log(Math.floor(num2)); // faqat butun qismini oberadi
+console.log(Math.abs(num3)); // modul
+var num4 = Math.max(45, 45, 75, 96, 12); // maximum qiymatini chiqazberadi min bolsa minimumini ciqazberadi
+console.log(num4);
+var max = 20;
+var min = 5;
+var num = Math.random() * (max - min); // ixtiyoriy sonni topib beradi
+console.log(Math.floor(num) + min);
+// -------------------3chi dars js --------------------
